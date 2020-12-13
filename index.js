4fcaa190890e665e17d31632a4810c69f4790f26
Glitch (glen-cheerful-babcat) <none>
2020-12-13 21:38:36 +0000

    ⚽️🐿 notify mod channel when someone adds appropriate emoji or @ the bot

--- index.js
+++ index.js
@@ -1,36 +1,60 @@
 const { App, LogLevel } = require("@slack/bolt");
 
 require("dotenv").config();
+
 const bot = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   token: process.env.SLACK_BOT_TOKEN,
   logLevel: LogLevel.debug
 });
 
-async function replyMessage(id, ts, msgText) {
+async function publishMessage(id, text, link) {
   try {
     const result = await bot.client.chat.postMessage({
-      // The token you used to initialize your app
-      token: bot.token,
+      token: process.env.SLACK_BOT_TOKEN,
       channel: id,
-      thread_ts: ts,
-      text: msgText
+      text: text + link
     });
-
-    // Print result
-    console.log(result);
   } catch (error) {
     console.error(error);
   }
 }
 
-bot.event("app_mention", async ({ event, say }) => {
+async function replyMessage(id, ts, msgText) {
   try {
-    if (event.thread_ts && event.text.includes("scam")) {
-      const msgText = `Hey <@${event.parent_user_id}>, Your post has been marked as suspicious.`;
-      await say(msgText);
-      await getPermalink({ event });
-      await notifyModChannel(event, msgText );
+    const result = await bot.client.chat.postMessage({
+      token: bot.token,
+      channel: id,
+      thread_ts: ts,
+      text: msgText
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+bot.event("app_mention", async ({ event, say, client }) => {
+  try {
+    // if the bot is tagged under a unique thread and the thread is not by the bot
+    if (
+      event.thread_ts &&
+      event.text.includes("scam") &&
+      event.parent_user_id !== "U01G9D4DX9Q"
+    ) {
+      const channelMsg = `Hey <@${event.parent_user_id}>, Your post has been marked as suspicious.`;
+
+      // call the user out in the same channel
+      await say(channelMsg);
+
+      const msgText = `A post by <@${event.parent_user_id}> in <#${event.channel}> was flagged by <@${event.user}> as suspicious. `;
+      // grab the link to the particular thread
+      const link = await client.chat.getPermalink({
+        token: client.token,
+        channel: event.channel,
+        message_ts: event.thread_ts
+      });
+      // notify the mod-group
+      publishMessage('modmode', msgText, link.permalink);
     } else {
       return;
     }
@@ -39,80 +63,35 @@ bot.event("app_mention", async ({ event, say }) => {
   }
 });
 
-// so far, only finds link to parent threads and not individual message replies
-const getPermalink = event => {
-  let link, messageTime;
-
-  async ({ event, client }) => {
-    try {
-      const messageLink = bot.client.chat.getPermalink({
-        token: bot.token,
-        channel: event.channel,
-        message_ts: event.thread_ts
-      });
-      console.log(messageLink);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-};
-
-const notifyModChannel = ( event, msgText ) => {
-  async ({ event, client }) => {
-    try {
-      client.chat.postMessage({
-        token: bot.token,
-        channel: event.channel,
-        text: msgText,
-        block: [
-          {
-            type: "section",
-            text: {
-              type: "plain_text",
-              text: msgText
-            },
-            accessory: {
-              type: "button",
-              text: {
-                type: "plain_text",
-                text: "View Post",
-                emoji: true
-              },
-              value: "click_me_123",
-              url: "https://google.com",
-              action_id: "button-action"
-            }
-          }
-        ]
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  };
-};
-
-// doesn't work yet
 bot.event("reaction_added", async ({ event, client }) => {
-  console.log(event);
-  if (event.reaction === ("scamalert" | "caution")) {
+  if (["scamalert","caution"].includes(event.reaction)) {
     try {
-      const msgText = `A post by <@${event.parent_user_id}> has been marked as suspicious.`;
-      // notifyModChannel( event, msgText );
+      const msgText =
+        `A post by <@${event.item_user}> in <#${event.channel}> was marked by <@${event.user}> as ` +
+        `:${event.reaction}:. `;
+
+      const link = await client.chat.getPermalink({
+        token: client.token,
+        channel: event.item.channel,
+        message_ts: event.item.ts
+      });
+
+      publishMessage('modmode', msgText, link.permalink);
     } catch (error) {
       console.error(error);
     }
   }
 });
 
+// doesn't work yet
 bot.command("/scam-alert", async ({ ack, payload, context }) => {
-  ack();
-
   console.log(payload);
+  ack();
 
   try {
     const result = await bot.client.chat.postMessage({
       token: bot.token,
-      channel: payload.channel_id,
+      channel: payload.channel,
       text:
         "Reminder from Your Friendly Neighborhood ModBot: Please don't give out any login credientials or banking information, exchange money or free labour for opportunities found here or job sites."
     });
